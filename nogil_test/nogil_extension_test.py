66330ce73ffdbc365eefcf9748913cbeb4612b46
Boxiang Sun <boxiang.sun@nexedi.com>
2018-12-31 11:18:44 +0100

    nogil extension ammending

--- nogil_test/nogil_extension_test.py
+++ nogil_test/nogil_extension_test.py
@@ -2,13 +2,11 @@ from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Build import cythonize
 
-ext_modules = [
-    Extension(
-        'nogil_extension',
-        ['nogil_extension.pyx'],
-    )
-]
-
 setup(
-    ext_modules = cythonize(ext_modules)
+    ext_modules = cythonize([
+        Extension(
+            'nogil_extension',
+            ['nogil_extension.pyx'],
+        ),
+    ])
 )
