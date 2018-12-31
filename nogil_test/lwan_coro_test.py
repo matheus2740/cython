4a25625132730e26cc009a2167bf321c349de4fc
Boxiang Sun <boxiang.sun@nexedi.com>
2018-12-31 10:28:17 +0100

    lwan coro test fixing

--- nogil_test/lwan_coro_test.py
+++ nogil_test/lwan_coro_test.py
@@ -8,7 +8,7 @@ def run(env, python_path):
   # build the extension
   # build_extension.build_ext(env)
   source = subprocess.check_output([python_path + " -c 'import wrapper; wrapper.main()'"],
-          cwd='./cython_lwan_coro/',
+          # cwd='./cython_lwan_coro/',
           env=env,
           shell=True,
           )
